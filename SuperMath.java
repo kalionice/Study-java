6f9722e5d984973f3541417cb2bbe6b2eeba58d3
jmw <84089306+kalionice@users.noreply.github.com>
2021-08-27 09:40:59 +0900

    Add files via upload

---
+++ SuperMath.java
@@ -0,0 +1,11 @@
+package com.MWcode;
+
+public class SuperMath {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int add(int a, int b, int c) {
+        return a + b + c;
+    }
+}
